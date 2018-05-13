adc6b3fcb5c17e076c4e5cfa13747e36b9f47d12
MarShin <martinshin95@gmail.com>
2018-05-13 16:52:02 +0800

    sucessfully upgrade project to RN 0.55 react 16.3.1 tgt with ios project file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import App from "./app/App";
 
-AppRegistry.registerComponent('Brainpick', () => App);
+AppRegistry.registerComponent("Brainpick", () => App);
