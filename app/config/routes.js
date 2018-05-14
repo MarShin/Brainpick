2f6a99d58730d47612c44a5d0d14d8c390050ad4
MarShin <martinshin95@gmail.com>
2018-05-14 15:40:22 +0800

    added eslint, redux and contribution page

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,73 +1,75 @@
-import React from "react";
-import {
-  createBottomTabNavigator,
-  createStackNavigator
-} from "react-navigation";
-import { Icon } from "react-native-elements";
+import React from 'react';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
-import Home from "../screens/Home";
-import CameraInstructions from "../screens/CameraInstructions";
+import Home from '../screens/Home';
+import CameraInstructions from '../screens/CameraInstructions';
 
-import Settings from "../screens/Settings";
-import Me from "../screens/Me";
+import Settings from '../screens/Settings';
+import Me from '../screens/Me';
+
+import Contribution from '../screens/Contribution';
 
 export const HomeStack = createStackNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
-      header: () => null // specifies no object return for header, default white space
-    }
+      header: () => null, // specifies no object return for header, default white space
+    },
   },
   CameraInstructions: {
     screen: CameraInstructions,
     navigationOptions: ({ navigation }) => ({
       // navigation object automatically attached to screen
-      headerTitle: "Camera Task"
-    })
-  }
+      headerTitle: 'Camera Task',
+    }),
+  },
 });
 
 export const Tabs = createBottomTabNavigator({
   Feed: {
     screen: HomeStack,
     navigationOptions: {
-      tabBarLabel: "Home",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="list" size={35} color={tintColor} />
-      )
-    }
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />,
+    },
+  },
+  Contribution: {
+    screen: Contribution,
+    navigationOptions: {
+      tabBarLabel: 'Me',
+      tabBarIcon: ({ tintColor }) => <Icon name="account-balance" size={35} color={tintColor} />,
+    },
   },
   Me: {
     screen: Me,
     navigationOptions: {
-      tabBarLabel: "Me",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="account-circle" size={35} color={tintColor} />
-      )
-    }
-  }
+      tabBarLabel: 'Me',
+      tabBarIcon: ({ tintColor }) => <Icon name="settings" size={35} color={tintColor} />,
+    },
+  },
 });
 
 export const SettingsStack = createStackNavigator({
   Settings: {
     screen: Settings,
     navigationOptions: {
-      title: "Settings"
-    }
-  }
+      title: 'Settings',
+    },
+  },
 });
 
 export const Root = createStackNavigator(
   {
     Tabs: {
-      screen: Tabs
+      screen: Tabs,
     },
     Settings: {
-      screen: SettingsStack
-    }
+      screen: SettingsStack,
+    },
   },
   {
-    mode: "modal",
-    headerMode: "none"
-  }
+    mode: 'modal',
+    headerMode: 'none',
+  },
 );
